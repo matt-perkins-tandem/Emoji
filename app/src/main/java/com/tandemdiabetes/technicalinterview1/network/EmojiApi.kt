b1a7661cdec3126ea81333dbff8b3b8404743970
Nick Foudi <156852367+nick-foudi-tandem@users.noreply.github.com>
2024-10-25 13:31:51 -0700

    Adjusting the project to use the emoji-api (#2)

--- app/src/main/java/com/tandemdiabetes/technicalinterview1/network/EmojiApi.kt
+++ app/src/main/java/com/tandemdiabetes/technicalinterview1/network/EmojiApi.kt
@@ -3,7 +3,7 @@ package com.tandemdiabetes.technicalinterview1.network
 import com.tandemdiabetes.technicalinterview1.domain.Emoji
 
 interface EmojiApi {
-    // TODO Implement the function fetchAllEmojis(). The end point lives at the following url: https://emojihub.yurace.pro/api/all
+    // TODO Implement the function fetchAllEmojis(). The end point lives at the following url: https://emoji-api.com/emojis?access_key=352a31dea84f985ea8f35049d2fb4d268310e14e
     // The function should perform the http call and retrieve the data, which will be a list of Emoji
 
     suspend fun fetchAllEmojis(): List<Emoji>
